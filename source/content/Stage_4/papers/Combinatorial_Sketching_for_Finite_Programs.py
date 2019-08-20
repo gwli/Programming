a9d4b52420cde8df48b8239138f9a086a3cf10fc
gwli <gwli@outlook.com>
2019-08-20 18:56:12 +0800

    syntax error

--- source/content/Stage_4/papers/Combinatorial_Sketching_for_Finite_Programs.py
+++ source/content/Stage_4/papers/Combinatorial_Sketching_for_Finite_Programs.py
@@ -1,6 +1,6 @@
 title = """Combinatorial Sketching for Finite Programs""""
 status = "done"
-url = http://delivery.acm.org/10.1145/1170000/1168907/p404-solar-lezama.pdf"
+url = "http://delivery.acm.org/10.1145/1170000/1168907/p404-solar-lezama.pdf"
 tags = ["program synthsis",
   ]
   
