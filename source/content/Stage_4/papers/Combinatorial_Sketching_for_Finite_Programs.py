9082921b2c7e6177dcf6a6f1f90dfb696f1bd7a4
gwli <gwli@outlook.com>
2019-08-20 18:54:54 +0800

    template for paper reading

---
+++ source/content/Stage_4/papers/Combinatorial_Sketching_for_Finite_Programs.py
@@ -0,0 +1,9 @@
+title = """Combinatorial Sketching for Finite Programs""""
+status = "done"
+url = http://delivery.acm.org/10.1145/1170000/1168907/p404-solar-lezama.pdf"
+tags = ["program synthsis",
+  ]
+  
+remarks = """
+
+"""
