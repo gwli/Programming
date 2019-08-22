6079c2be7177b18040f2a511d29ab96951901626
gwli <gwli@outlook.com>
2019-08-22 20:01:15 +0800

    符号执行的意义

--- source/content/Stage_4/papers/Finding_Code_That_Explodes_under_Symbolic_Evaluation.py
+++ source/content/Stage_4/papers/Finding_Code_That_Explodes_under_Symbolic_Evaluation.py
@@ -1,5 +1,13 @@
 title="Finding Code That Explodes under Symbolic Evaluation"
-status="new"
+status="ongoing"
 tags = ["profiling synthesis tools",]
+rank = ""
 remarks = """ 
+
+Solver-aided tools 依赖符号执行，进行验证，合成。
+symbolic evaluation & symbolic exection 本身基于callstack,heatstack profiling. 
+
+解决符号建模时候瓶颈问题，而符号运算本身就是 量化优化目标算法，symProf 相当于对于建模工具的优化。
+
+符号对算法执行分本身进行建模，可以得到其精确的多样式，并且一系列的优化。
 """
