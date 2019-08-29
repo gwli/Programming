b6c4692a35bcfdfe49f5ec166836560723d9ff58
gwli <gwli@outlook.com>
2019-08-29 16:54:15 +0800

    Create A_Framework_for_Computer-Aided_Design_of_Educational_Domain_Models.py

---
+++ source/content/Stage_4/papers/A_Framework_for_Computer-Aided_Design_of_Educational_Domain_Models.py
@@ -0,0 +1,7 @@
+title = "A Framework for Computer-Aided Design of Educational Domain Models"
+url= "https://homes.cs.washington.edu/~emina/doc/rulesy.vmcai18.pdf"
+code = "https://github.com/edbutler/nonograms-rule-synthesis"
+status= ""
+tags = []
+remarks = """
+"""
