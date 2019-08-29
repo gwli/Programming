69dbb5646f1af1a01b781bf6d6e548053303ef1a
gwli <gwli@outlook.com>
2019-08-29 18:49:29 +0800

    Create DeepCoder.py

---
+++ source/content/Stage_4/papers/DeepCoder.py
@@ -0,0 +1,14 @@
+title= "Deep Coder"
+
+url= "https://arxiv.org/abs/1611.01989"
+code = "https://github.com/dkamm/deepcoder"
+status=""
+tags = ["deepcoder","IPS"]
+remarks = """
+
+这是一篇不错解读，
+https://blog.csdn.net/qq_15192373/article/details/89052205
+
+一阶函数：HEAD，LAST，TAKE，DROP，ACCESS，MINIMUM，MAXIMUM，REVERSE，SORT，SUM
+高阶函数：MAP，FILTER，COUNT，ZIPWITH，SCANL1
+"""
