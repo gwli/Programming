0265e2d9f37a1bff21cb2fefa41996ab8da201fa
gwli <gwli@outlook.com>
2019-08-29 16:07:24 +0800

    Create learning_to_infer_graphics_programs_from_hand_drawn_images.py

---
+++ source/content/Stage_4/papers/learning_to_infer_graphics_programs_from_hand_drawn_images.py
@@ -0,0 +1,3 @@
+title="learning-to-infer-graphics-programs-from-hand-drawn-images.pdf"
+url="https://papers.nips.cc/paper/7845-learning-to-infer-graphics-programs-from-hand-drawn-images.pdf"
+code = "https://github.com/ellisk42/TikZ"
