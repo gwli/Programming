aa3fe6ff66152291f1d9711474313c2fe4c53fa2
gwli <gwli@outlook.com>
2019-08-29 16:12:38 +0800

    手写到代码

--- source/content/Stage_4/papers/learning_to_infer_graphics_programs_from_hand_drawn_images.py
+++ source/content/Stage_4/papers/learning_to_infer_graphics_programs_from_hand_drawn_images.py
@@ -1,3 +1,9 @@
 title="learning-to-infer-graphics-programs-from-hand-drawn-images.pdf"
 url="https://papers.nips.cc/paper/7845-learning-to-infer-graphics-programs-from-hand-drawn-images.pdf"
 code = "https://github.com/ellisk42/TikZ"
+tags = ["Program Sythsis"]
+status = "close"
+remarks = """
+把手写的图片，生成letex的图形片断，并且还能反向画来。 这个要比图片公式变成latex公式，要复杂多了，因为这些一些数量与位置关系，并且还要生成
+带有loop,if 的代码断，生成时利用了SKetch 1.75来实现。
+"""
