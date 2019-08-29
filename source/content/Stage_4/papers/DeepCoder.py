ba5c6ed8aaceb6045a1e616ec5f7881de8efc9a4
gwli <gwli@outlook.com>
2019-08-29 19:56:32 +0800

    Update DeepCoder.py

--- source/content/Stage_4/papers/DeepCoder.py
+++ source/content/Stage_4/papers/DeepCoder.py
@@ -11,4 +11,6 @@ https://blog.csdn.net/qq_15192373/article/details/89052205
 
 一阶函数：HEAD，LAST，TAKE，DROP，ACCESS，MINIMUM，MAXIMUM，REVERSE，SORT，SUM
 高阶函数：MAP，FILTER，COUNT，ZIPWITH，SCANL1
+以用于自行编写代码并解决简单的数学问题。这样是不是可以准备一些让其学习。目前只能写出 5 行左右的程序代码，难度相当于程序竞赛网站中最简单的问题
+https://github.com/microsoft/DeepCoder-Utils
 """
