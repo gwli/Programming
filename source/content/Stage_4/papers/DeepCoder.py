22bd86f9859abf45fdf36df26f1aec34d5baece7
gwli <gwli@outlook.com>
2019-08-30 16:02:22 +0800

    Update DeepCoder.py

--- source/content/Stage_4/papers/DeepCoder.py
+++ source/content/Stage_4/papers/DeepCoder.py
@@ -13,4 +13,7 @@ https://blog.csdn.net/qq_15192373/article/details/89052205
 高阶函数：MAP，FILTER，COUNT，ZIPWITH，SCANL1
 以用于自行编写代码并解决简单的数学问题。这样是不是可以准备一些让其学习。目前只能写出 5 行左右的程序代码，难度相当于程序竞赛网站中最简单的问题
 https://github.com/microsoft/DeepCoder-Utils
+
+形成 attributes array,也就是各个原语概率分布。 然后这个概率分布来加速搜索的速度。
+https://www.zhinengl.com/2017/03/microsoft-ai-deepcoder/
 """
